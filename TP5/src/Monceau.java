6974ee9940c2f7c1a281eda7d3933208000194fd
siminot <turcotte.simon@hotmail.com>
2017-12-01 14:43:18 -0500

    Ajout commentaire des methodes de Monceau

--- TP5/src/Monceau.java
+++ TP5/src/Monceau.java
@@ -7,23 +7,47 @@ public class Monceau {
     public Monceau() {
         arbres = new ArrayList<Node>();
     }
-    
+
+
+    /**
+     * Permet fusionner deux monceaux ensemble
+     *
+     * @param  autre : monceau avec lequel faire la fusion
+     * @return void
+     */
     public void fusion(Monceau autre) {
         // à compléter
     }
-    
+
+    /**
+     * Permet d'insérer une valeur dans le monceau
+     *
+     * @param  val : valeur à insérer
+     * @return void
+     */
     public void insert(int val) {
         // à compléter
     }
-    
+
+    /**
+     * Permet de retrouver un noeud avec la valeur passée en paramètre
+     *
+     * @param  val : valeur du noeud à trouver
+     * @return boolean : si le monceau possède la valeur
+     */
     public boolean delete (int val) {
         // à compléter
         
         return false;
     }
-    
+
+
+    /**
+     * Permet d'afficher les éléments du monceau
+     *
+     * @return void
+     */
     public void print() {
         // à compléter
     }
-    
 }
