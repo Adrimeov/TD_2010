cf5bfdf495a002c4abfb5f64f03ce93587bfc4a7
Adrimeov <jffontaine_@hotmail.com>
2017-12-05 10:33:58 -0500

    premier numero

--- TP6/src/CircularDeps/CodeBase.java
+++ TP6/src/CircularDeps/CodeBase.java
@@ -25,6 +25,25 @@ public class CodeBase {
     // des dépendances circulaires dans les fichiers source.
     private boolean existsCircularDependencies(SourceFile file, HashSet<SourceFile> dependentFiles)
     {
+
+        //On ajoute le fichier a l'etude dans le set de verification
+        dependentFiles.add(file);
+        //On itere sur les enfants
+        for(SourceFile files: file.getDependencies() ){
+            //Si l'enfant n'est pas deja dans le set, on passe le meme processus a celui-ci
+            if(!dependentFiles.contains(files)) {
+                existsCircularDependencies(files, dependentFiles);
+            }
+            //Si l'enfant est dans la liste on doit remonter la pile d'execution
+            if(dependentFiles.contains(files)){
+                return true;
+            }
+
+        }
+        // Si aucune boucle, on retire l'un apres l'autre. C'est dans le cas
+        // que nous sommes descendu toute en profondeur.
+        dependentFiles.remove(file);
+
         // À compléter
 
         return false;
